1aafa3f8fb3777c6d89efec9d4ac2466d1877022
KAU5H4L <pacharne.ai21@bmsce.ac.in>
2023-08-18 11:22:35 +0530

    hackerrank2.cpp

---
+++ hackerrank2.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    double nrm=0.5;
+    double nre=0.4;
+    double both=0.2;
+    double rm=1-nrm;
+    double re=1-nre;
+    double x =(rm+re-both);
+    cout<<"9/10";
+    return 0;
+}
